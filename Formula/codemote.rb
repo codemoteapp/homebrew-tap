1da37cb4f9f33d6dceb2f07f6b425d89b111f32c
Codemote <hello@codemote.app>
2026-03-29 19:31:33 +0300

    Update formula to v1.3.8 with embedded relay

--- Formula/codemote.rb
+++ Formula/codemote.rb
@@ -1,20 +1,20 @@
 class Codemote < Formula
   desc "Control AI coding assistants from your iPhone"
   homepage "https://github.com/codemoteapp"
-  version "1.3.7"
+  version "1.3.8"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/codemoteapp/codemote-/releases/download/v1.3.7/codemote-1.3.7-macos-arm64.tar.gz"
-      sha256 "d1b3e7b936f7ea16a930c4e53da58ab81349678007e8996359444cfc95313fcf"
+      url "https://github.com/codemoteapp/codemote-/releases/download/v1.3.8/codemote-1.3.8-macos-arm64.tar.gz"
+      sha256 "cca7f2efaaeab9887d4bd09dc843d502abd87263db7b87ffea516f46077090c8"
 
       def install
         bin.install "codemote-arm64" => "codemote"
       end
     else
-      url "https://github.com/codemoteapp/codemote-/releases/download/v1.3.7/codemote-1.3.7-macos-x64.tar.gz"
-      sha256 "6d0df82d7a33bc632a5a9d01ba1e7ead03bb8cbe02952e2c53216e89abc97fdd"
+      url "https://github.com/codemoteapp/codemote-/releases/download/v1.3.8/codemote-1.3.8-macos-x64.tar.gz"
+      sha256 "3e5ff9c73a90ad69d23344ffa18aef4f266caf335af5b51e84b0ceffc1cd79c0"
 
       def install
         bin.install "codemote-x64" => "codemote"
