639c81b023da6c1d8212af30d1ee190d86a2cd5f
Samet Ak <sametak@Samet-MacBook-Pro.local>
2026-03-30 16:33:36 +0300

    Update codemote formula to 1.4.2

--- Formula/codemote.rb
+++ Formula/codemote.rb
@@ -1,20 +1,20 @@
 class Codemote < Formula
   desc "Control AI coding assistants from your iPhone"
   homepage "https://github.com/codemoteapp"
-  version "1.4.1"
+  version "1.4.2"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/codemoteapp/codemote-/releases/download/v1.4.1/codemote-1.4.1-macos-arm64.tar.gz"
-      sha256 "8d7f5be37fe609bf9abf8edec7c754f9a11177dd573da00c5d4be2f7d1a95f2c"
+      url "https://github.com/codemoteapp/codemote-/releases/download/v1.4.2/codemote-1.4.2-macos-arm64.tar.gz"
+      sha256 "fc3a193eed7ccc92af30620e815002633298c8f6006e8e87cfde33d98e824d1c"
 
       def install
         bin.install "codemote-arm64" => "codemote"
       end
     else
-      url "https://github.com/codemoteapp/codemote-/releases/download/v1.4.0/codemote-1.4.0-macos-x64.tar.gz"
-      sha256 "9725c5ee4df148064a606c54202eb340b50f1aa5c14cb81c68a9b1afe10b57b3"
+      url "https://github.com/codemoteapp/codemote-/releases/download/v1.4.2/codemote-1.4.2-macos-x64.tar.gz"
+      sha256 "16a6a862c06e23dea1e071b7888c430f846d6c29b344421b32c997fad0247829"
 
       def install
         bin.install "codemote-x64" => "codemote"
